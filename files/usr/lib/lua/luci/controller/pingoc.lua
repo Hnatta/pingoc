d941c07a6a2c8465118b1be4db5fa1e42bc9a086
Hnatta <66830227+Hnatta@users.noreply.github.com>
2025-10-01 17:32:52 +0700

    Update pingoc.lua

--- files/usr/lib/lua/luci/controller/pingoc.lua
+++ files/usr/lib/lua/luci/controller/pingoc.lua
@@ -1,4 +1,5 @@
 module("luci.controller.pingoc", package.seeall)
 function index()
-entry({"admin","services","pingoc"}, template("pingoc"), _("Ping OC"), 3).leaf=true
+entry({"admin","services","pingoc"}, template("pingoc"), _("Ping OC"), 95).leaf=true
+
 end
